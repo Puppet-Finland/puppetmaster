628155a5f3ba9aa5bdebf321db8fd4210bd66307
Samuli Seppänen <samuli@openvpn.net>
2013-06-25 16:17:10 +0300

    Initial import to GitHub
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

---
+++ manifests/init.pp
@@ -0,0 +1,21 @@
+#
+# == Class: puppetmaster
+#
+# Configure various aspects of a puppetmaster. Currently only manages 
+# iptables/ip6tables rules through use of the puppetmaster::allow define.
+#
+# == Authors
+#
+# Samuli Seppänen <samuli@openvpn.net>
+#
+# == License
+#
+# BSD-lisence
+# See file LICENSE for details
+#
+class puppetmaster {
+
+    # This class does nothing at the moment, but is still needed for the 
+    # puppetmaster::allow define.
+
+}
