cd8460ba2e9f286fc9d7e9162021d66446950352
Samuli Seppänen <samuli@openvpn.net>
2013-09-13 19:52:38 +0000

    Added default values to Hiera checks
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- manifests/init.pp
+++ manifests/init.pp
@@ -17,7 +17,7 @@
 class puppetmaster {
 
 # Rationale for this is explained in init.pp of the sshd module
-if hiera('manage_puppetmaster') != 'false' {
+if hiera('manage_puppetmaster', 'true') != 'false' {
 
     # This class does nothing at the moment, but is still needed for the 
     # puppetmaster::allow define.
