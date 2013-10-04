f486bde643a22e332b809729b8a7339afa8b72fb
Samuli Seppänen <samuli@openvpn.net>
2013-10-04 09:00:54 +0000

    Added management of getip.sh to puppetmaster class
    This script is used to generate (usually public) IP addresses for the nodes
    based on their $fqdn fact. This is used by some modules such as
    bacula::filedaemon when they exported firewall resources, and the $ipaddress
    fact is useless because it points to a private IP address.
    
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- manifests/init.pp
+++ manifests/init.pp
@@ -19,8 +19,19 @@ class puppetmaster {
 # Rationale for this is explained in init.pp of the sshd module
 if hiera('manage_puppetmaster', 'true') != 'false' {
 
-    # This class does nothing at the moment, but is still needed for the 
-    # puppetmaster::allow define.
+    # Install a script used for checking a node's IP at the puppetmaster side. 
+    # This is useful/necessary if using exported (firewall) resources and the 
+    # $ipaddress facts are useless. This is the case, for example, when a public 
+    # IP address is needed, but a private IP is returned as $ipaddress. This is 
+    # the case on Amazon EC2, for example.
 
+    file { 'puppetmaster-getip.sh':
+        name => '/usr/local/bin/getip.sh',
+        ensure => present,
+        content => template('puppetmaster/getip.sh.erb'),
+        owner => root,
+        group => root,
+        mode => 755,
+    }
 }
 }
