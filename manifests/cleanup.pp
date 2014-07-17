758e8a61561497c4c20ed76ae98cc1e061969a58
Samuli Seppänen <samuli@openvpn.net>
2014-07-17 08:22:51 +0000

    Added support for cleaning old puppet reports via puppetmaster::cleanup class
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

---
+++ manifests/cleanup.pp
@@ -0,0 +1,40 @@
+#
+# == Class: puppetmaster::cleanup
+#
+# Add various cleanup tasks to cron.
+#
+# == Parameters
+#
+# [*max_report_age*]
+#   Maximum age in days for puppet agent reports. These can consume tons of 
+#   diskspace, so keeping this value fairly small (1-4 weeks?) usually makes 
+#   sense. Defaults to 7.
+# [*hour*]
+#   Hour(s) when the checks are run. Defaults to '12'.
+# [*minute*]
+#   Minute(s) when the checks are run. Defaults to '15'.
+# [*weekday*]
+#   Weekday(s) when the cheks are run. Defaults to * (all weekdays).
+# [*email*]
+#   Email address where notifications are sent. Defaults to top-scope variable
+#   $::servermonitor.
+#
+class puppetmaster::cleanup
+(
+    $max_report_age = 7,
+    $hour = '12',
+    $minute = '15',
+    $weekday = '*',
+    $email = $::servermonitor
+)
+{
+    cron { 'puppetmaster-clean-reports':
+        ensure => present,
+        command => "find /var/lib/puppet/reports -name \"*.yaml\" -mtime +${max_report_age} -exec rm -f {} \; > /dev/null",
+        user => root,
+        hour => $hour,
+        minute => $minute,
+        weekday => $weekday,
+        environment => "MAILTO=${email}",
+    }
+}
