47b233d7527ca01207caef23f0869de600677b07
Samuli Seppänen <samuli.seppanen@gmail.com>
2015-05-08 12:01:50 +0000

    Added support for RedHat
    Signed-off-by: Samuli Seppänen <samuli.seppanen@gmail.com>

--- manifests/params.pp
+++ manifests/params.pp
@@ -6,4 +6,16 @@
 class puppetmaster::params {
 
     include ::os::params
+
+    case $::osfamily {
+        'RedHat': {
+            $json_check_cmd = 'python -m json.tool'
+        }
+        'Debian': {
+            $json_check_cmd = 'json_pp -f json -t null'
+        }
+        default: {
+            fail("Unknown operating system ${::osfamily}")
+        }
+    }
 }
